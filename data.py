76dec20076e430e274a7ab91e5928c9b40ea22d1
erdag <erdag@rocketmail.com>
2018-06-28 14:58:10 -0700

    Add files via upload

--- data.py
+++ data.py
@@ -2,27 +2,24 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('1625Data.txt',header=None)
-
-print(df.head())
-
 catalog = 'ARNDCQEGHILKMFPSTWYV'
 
-
-
-def vectorize(protein):
-    result = np.zeros((1,20),dtype='uint8')
-    for p in protein:
-        
-        indice = catalog.find(p)
-        result[0][indice] = 1
-    return result
+df_train = pd.DataFrame(np.zeros((1625,120), dtype='uint8'))
 
 
 
+protein_count = 0
+for protein in df[0]:
+    print(protein)
+    letter_count = 0
+    for letter in protein:
+        print('protein count: ' + str(protein_count))
+        print('letter count: ' + str(letter_count))
+        print(catalog.find(letter))
+        catalog_indice = catalog.find(letter)
+        df_train.iloc[protein_count][letter_count*20 + catalog_indice] = 1
+        letter_count += 1
+    protein_count += 1 
+            
 
-df['train_data'] = df[0].apply(vectorize)
-
-print(df['train_data'][0])
-print(df['train_data'][1])
-print(df['train_data'][2])
-print(df['train_data'][3])
+# df_train.iloc[2:3,0:20] particular row
